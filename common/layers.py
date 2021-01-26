625ba9ef75ce44695a1037f566e5045d08d8c155
rnakamura-sky <rnakamura1234sky@gmail.com>
2021-01-27 08:21:14 +0900

    1章更新

---
+++ common/layers.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+class MatMul:
+    def __init__(self, W):
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]
+        self.x = None
+    
+    def forward(self, x):
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.params = []
+        self.grads = []
+        self.out = None
+    
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+    
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
+        return dx
+
