a88f0c5b0fbc8c44707fb9c1cd93f2436507a7a6
rnakamura-sky <rnakamura1234sky@gmail.com>
2021-01-28 07:56:30 +0900

    1章更新

--- common/layers.py
+++ common/layers.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from common.functions import softmax, cross_entropy_error
+
 class MatMul:
     def __init__(self, W):
         self.params = [W]
@@ -34,3 +36,66 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
         return dx
 
+class Affine:
+    def __init__(self, W, b):
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+    
+    def forward(self, x):
+        W, b = self.params
+        out = np.dot(x, W) + b
+        self.x = x
+        return out
+    
+    def backward(self, dout):
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+
+        return dx
+
+class Softmax:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+    
+    def forward(self, x):
+        self.out = softmax(x)
+        return self.out
+    
+    def backward(self, dout):
+        dx = self.out * dout
+        sumdx = np.sum(dx, axis=1, keepdims=True)
+        dx -= self.out * sumdx
+        return dx
+    
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        if self.t.size == self.y.size:
+            self.t = self.t.argmax(axis=1)
+        
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+
+        dx = self.y.copy()
+        dx[np.arange(batch_size, self.t)] -= 1
+        dx *= dout
+        dx = dx / batch_size
+
+        return dx
