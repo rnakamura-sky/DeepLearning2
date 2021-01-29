e5c4a343febe2aca64408d311745b374862fae57
rnakamura-sky <rnakamura1234sky@gmail.com>
2021-01-29 12:38:51 +0900

    1章 スパイラル学習

--- ch01/two_layer_net.py
+++ ch01/two_layer_net.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('.')
+sys.path.append('..')
 import numpy as np
 from common.layers import Affine, Sigmoid, SoftmaxWithLoss
 
@@ -15,7 +15,7 @@ class TwoLayerNet:
         self.layers = [
             Affine(W1, b1),
             Sigmoid(),
-            Affine(W2, b2),
+            Affine(W2, b2)
         ]
         self.loss_layer = SoftmaxWithLoss()
 
@@ -23,20 +23,19 @@ class TwoLayerNet:
         for layer in self.layers:
             self.params += layer.params
             self.grads += layer.grads
-    
+
     def predict(self, x):
         for layer in self.layers:
             x = layer.forward(x)
         return x
-    
+
     def forward(self, x, t):
         score = self.predict(x)
         loss = self.loss_layer.forward(score, t)
         return loss
-    
+
     def backward(self, dout=1):
         dout = self.loss_layer.backward(dout)
         for layer in reversed(self.layers):
             dout = layer.backward(dout)
         return dout
-    
--- common/layers.py
+++ common/layers.py
@@ -7,13 +7,13 @@ class MatMul:
         self.params = [W]
         self.grads = [np.zeros_like(W)]
         self.x = None
-    
+
     def forward(self, x):
         W, = self.params
         out = np.dot(x, W)
         self.x = x
         return out
-    
+
     def backward(self, dout):
         W, = self.params
         dx = np.dot(dout, W.T)
@@ -21,33 +21,18 @@ class MatMul:
         self.grads[0][...] = dW
         return dx
 
-class Sigmoid:
-    def __init__(self):
-        self.params = []
-        self.grads = []
-        self.out = None
-    
-    def forward(self, x):
-        out = 1 / (1 + np.exp(-x))
-        self.out = out
-        return out
-    
-    def backward(self, dout):
-        dx = dout * (1.0 - self.out) * self.out
-        return dx
-
 class Affine:
     def __init__(self, W, b):
         self.params = [W, b]
         self.grads = [np.zeros_like(W), np.zeros_like(b)]
         self.x = None
-    
+
     def forward(self, x):
         W, b = self.params
         out = np.dot(x, W) + b
         self.x = x
         return out
-    
+
     def backward(self, dout):
         W, b = self.params
         dx = np.dot(dout, W.T)
@@ -56,46 +41,59 @@ class Affine:
 
         self.grads[0][...] = dW
         self.grads[1][...] = db
-
         return dx
 
 class Softmax:
     def __init__(self):
         self.params, self.grads = [], []
         self.out = None
-    
+
     def forward(self, x):
         self.out = softmax(x)
         return self.out
-    
+
     def backward(self, dout):
         dx = self.out * dout
         sumdx = np.sum(dx, axis=1, keepdims=True)
         dx -= self.out * sumdx
         return dx
-    
+
 class SoftmaxWithLoss:
     def __init__(self):
         self.params, self.grads = [], []
         self.y = None
         self.t = None
-    
+
     def forward(self, x, t):
         self.t = t
         self.y = softmax(x)
 
         if self.t.size == self.y.size:
             self.t = self.t.argmax(axis=1)
-        
+
         loss = cross_entropy_error(self.y, self.t)
         return loss
-    
+
     def backward(self, dout=1):
         batch_size = self.t.shape[0]
 
         dx = self.y.copy()
-        dx[np.arange(batch_size, self.t)] -= 1
+        dx[np.arange(batch_size), self.t] -= 1
         dx *= dout
         dx = dx / batch_size
 
+        return dx
+
+class Sigmoid:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        dx = dout * (1.0 - self.out) * self.out
         return dx
