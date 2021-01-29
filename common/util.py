7c39c76d798e51294c89bd02b98e9ed34876af9e
rnakamura-sky <rnakamura1234sky@gmail.com>
2021-01-29 17:35:15 +0900

    Revert "2しょ"
    This reverts commit 5f8a269843288bfc36fa0536a939e89e90da5c82.

--- common/util.py
+++ common/util.py
@@ -17,11 +17,6 @@ def preprocess(text):
     corpus = np.array([word_to_id[w] for w in words])
     return corpus, word_to_id, id_to_word
 
-def cos_similarity(x, y, eps=1e-8):
-    nx = x / (np.sqrt(np.sum(x**2)) + eps)
-    ny = y / (np.sqrt(np.sum(y**2)) + eps)
-    return np.dot(nx, ny)
-
 def create_co_matrix(corpus, vocab_size, window_size=1):
     corpus_size = len(corpus)
     co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
